26d6297c8f4ff04ce93974163e0a73f9d48a1d19
liseth giraldo <70545096+lisethgira@users.noreply.github.com>
2023-09-14 15:42:59 -0500

    Update bodytech.php

--- bodytech.php
+++ bodytech.php
@@ -16,7 +16,7 @@
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav mr-auto">
                         <li class="nav-item active">
-                            <a class="nav-link" href="index.php">Inicio <span class="sr-only">(current)</span></a>
+                            <a class="nav-link" href="index.html">Inicio <span class="sr-only">(current)</span></a>
                         </li>
                         <li class="nav-item active">
                             <a class="nav-link" href="calculadora.php">Calculadora</a>
